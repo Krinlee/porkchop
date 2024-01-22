14d01588ede0a790fdb23745972759ed4d190e2a
Krinlee <mrkrinlee@gmail.com>
2024-01-21 21:10:35 -0500

    hope this goes well

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, settings, random, asyncio, utils
+import discord, settings, random, asyncio
 from discord.ext import commands
 from discord import app_commands
 from settings import *
